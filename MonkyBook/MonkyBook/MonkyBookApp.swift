294e743f5da4b1b2161290e1df80f53f49fa35e4
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-06-24 15:49:17 -0300

    create/project
    Criando o projeto Xcode

---
+++ MonkyBook/MonkyBook/MonkyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkyBookApp.swift
+//  MonkyBook
+//
+//  Created by Caio Marques on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MonkyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
