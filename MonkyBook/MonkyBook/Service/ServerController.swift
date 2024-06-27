aaf9596cfc549a3f4051474ae788c57480e291e4
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-06-27 15:25:43 -0300

    style/comentando-codigo-server-controller

--- MonkyBook/MonkyBook/Service/ServerController.swift
+++ MonkyBook/MonkyBook/Service/ServerController.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
+/// Uma classe que basicamente faz todas as requisições necessárias ao banco de dados
 class ServerController {
     let urlString = "http://localhost:8080/books"
     
-    func fetchBooks () async throws -> [Book]?  {
+    private func request (_ link : String) throws -> URLRequest {
+        // verificando se a url existe, se não jogando um erro de URL ruim
         guard let url = URL(string: self.urlString) else {
             throw URLError(.badURL)
         }
-        let request = URLRequest(url: url)
+        return URLRequest(url: url)
+    }
+    
+    /// Função que recupera todos os livros dentro da API.
+    /// - returns: lista de todos os books dentro da API.
+    func fetchBooks () async throws -> [Book]?  {
+        let request = try request(urlString)
         
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
@@ -26,13 +34,12 @@ class ServerController {
         }
     }
     
+    /// Função que recupera um dos livros dentro da API a partir de um id
+    /// - Parameters:
+    ///     - id: id do livro que se deseja recuperar.
+    /// - returns: o book buscado.
     func getBook (_ id : UUID) async throws -> Book? {
-        let urlString = self.urlString + "/\(id)"
-        
-        guard let url = URL(string: urlString) else {
-            throw URLError(.badURL)
-        }
-        let request = URLRequest(url: url)
+        let request = try request(urlString)
         
         do {
             let (data, _) = try await URLSession.shared.data(for: request)
@@ -44,15 +51,13 @@ class ServerController {
         }
     }
     
-    /// Uma função que vai adicionar um book e vai retornar o book criado e uma resposta em inteiro se a transação funcionou ou não
+    /// Uma função que vai adicionar um book no banco de dados da API
+    /// - Parameters:
+    ///     - book: objeto do tipo Book que se deseja adicionar
+    /// - returns: o book gerado e um inteiro que representa o status code da requisição. Ambos opcionais caso a inserção não aconteça
     func addBook (_ book : Book) async throws -> (Book?, Int?) {
-        // verificando se a url existe, se não jogando um erro de URL ruim
-        guard let url = URL(string: urlString) else {
-            throw URLError(.badURL)
-        }
-        
         // configurando a requisição
-        var request = URLRequest(url: url)
+        var request = try request(urlString)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
@@ -72,17 +77,13 @@ class ServerController {
         return (bookDecoded, statusCode)
     }
     
+    /// Uma função que remove um book no banco de dados da API
+    /// - Parameters:
+    ///     - id: id do livro que se deseja remover
+    /// - returns: Um inteiro que representa o status code da requisição.
     func removeBook (_ id : UUID) async throws -> Int? {
-        let urlString = urlString + "/\(id)"
-        
-        // verificando se a url existe, se não jogando um erro de URL ruim
-        guard let url = URL(string: urlString) else {
-            throw URLError(.badURL)
-        }
-        
-        var request = URLRequest(url: url)
+        var request = try request(urlString + "/\(id)")
         request.httpMethod = "DELETE"
-        //request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let (_, response) = try await URLSession.shared.data(for: request)
         
@@ -90,13 +91,13 @@ class ServerController {
         return responseHTTP?.statusCode
     }
     
+    /// Uma função que atualiza um book
+    /// - Parameters:
+    ///     - id: id do livro que se deseja atualizar
+    ///     - book: um objeto do tipo Book que vai conter as informações que vão ser alteradas, como nome e autor
+    /// - returns: Um inteiro que representa o status code da requisição.
     func updateBook (_ id : UUID, book: Book) async throws -> Int?  {
-        let urlString = self.urlString + "/\(id)"
-        
-        guard let url = URL(string: urlString) else {
-            throw URLError(.badURL)
-        }
-        var request = URLRequest(url: url)
+        var request = try request(self.urlString + "/\(id)")
         request.httpMethod = "PUT"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
